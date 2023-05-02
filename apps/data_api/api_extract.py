29f86976ae994d221c62390b1c40cecb1955886a
pstar314 <ckdals754@gmail.com>
2023-05-03 00:15:53 +0900

    feat : Add data extraction from api

---
+++ apps/data_api/api_extract.py
@@ -0,0 +1,73 @@
+from env import key
+import requests
+import json
+import pandas as pd
+from datetime import datetime
+
+"""
+openAPI로부터 drugs 데이터 추출
+
+조건
+- 데이터 갯수 316개(json.body.totalCount)
+- API 호출 시 한번에 가져올 수 있는 데이터 수 100개
+- 4번 호출하여 316개 추출 후 concat
+- 데이터 경로 : json.body.items
+- null인 경우 : ""
+"""
+
+def get_All_Data(key, url):
+    data = []
+    params = {'serviceKey' : key, 'pageNo' : '1', 'numOfRows' : '1', 'Drfstf' : '', 'Drfstf_eng' : '', 'type' : 'json'}
+    
+    # 전체 데이터 수 탐색
+    loading = json.loads(requests.get(url, params=params).text)
+    cnt = loading['body']['totalCount']
+    
+    pages = (cnt // 100) + 1
+    params['numOfRows'] = str(100)
+    loading['body']['items'].pop()
+    all_json = loading
+    
+    for i in range(1, pages + 1):
+        if i == pages:
+            params['numOfRows'] = str(cnt % 100)
+        params['pageNo'] = str(i)
+        sub_data = json.loads(requests.get(url, params=params).text)
+        data += sub_data['body']['items']
+    all_json['body']['items'] = data
+    return data, all_json
+
+def raw_2_modelformat(all_data):
+    all_data1 = all_data[:]
+    for i in range(len(all_data1)):
+        all_data1[i]['DEL_FIELD'] = False
+        all_data1[i]['ID'] = i+1
+        all_data1[i]['created_at'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        all_data1[i]['updated_at'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        
+    all_data2 = []
+    for i in range(len(all_data1)):
+        dic = {}
+        for key, value in all_data1[i].items():
+            lowkey = key.lower()
+            dic[lowkey] = all_data1[i][key]
+        all_data2.append(dic)
+    
+    jslist = []
+    for i in range(len(all_data1)):
+        dic = {"model":'data_api.Drug', 'fields':all_data2[i]}
+        jslist.append(dic)
+    
+    with open('./drug.json', 'w', encoding='UTF-8-sig') as f:
+        json.dump(jslist, f, indent=4,ensure_ascii=False)
+    
+
+def main():
+    apikey = key.API_CONFIG['key']
+    url = key.API_CONFIG['url']
+
+    all_data, all_json = get_All_Data(apikey, url)
+    raw_2_modelformat(all_data)
+
+if __name__ == "__main__":
+    main()
