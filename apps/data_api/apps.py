94bb30933df9c8e1975e0d0e6d762b23cb9ee2d2
MACLSH <gracia10@daum.net>
2023-05-03 15:48:14 +0900

    Fix: make load drug data fail message when server run

--- apps/data_api/apps.py
+++ apps/data_api/apps.py
@@ -1,8 +1,11 @@
-import json
+import logging
+from sqlite3 import OperationalError
+from django.db.utils import OperationalError
 
 from django.apps import AppConfig
-from django_seed import Seed
 from django.core.management import call_command
+
+logger = logging.getLogger(__name__)
 
 
 class DataApiConfig(AppConfig):
@@ -10,9 +13,12 @@ class DataApiConfig(AppConfig):
     name = 'apps.data_api'
 
     def ready(self):
-        # 서버가 실행될 때 Drug 모델에 데이터 생성
+        # ready 실행시 Drug 모델이 생성안될 수 있어 내부에서 호출
         from .models import Drug
-        if Drug.objects.exists():
-            Drug.objects.all().delete()
 
-        call_command('loaddata', 'drug.json')
+        try:
+            if Drug.objects.exists():
+                Drug.objects.all().delete()
+                call_command('loaddata', 'drug.json')
+        except OperationalError:
+            logger.warning("[drugs 데이터 생성 실패] drugs 테이블이 없습니다 migrate를 수행해주세요")
