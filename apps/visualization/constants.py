6712e408d403e1dc794a3832a8fc01c25c40276b
MACLSH <gracia10@daum.net>
2023-05-04 08:24:50 +0900

    feat : add visualization constants
    - app_name , error message 상수 추가

--- apps/visualization/constants.py
+++ apps/visualization/constants.py
@@ -1,2 +1,9 @@
+APP_NAME = 'chart'
 TOP10_PIE_CHART = 'top10_pie_chart'
 TOP10_CHART_TITLE = '자주 언급되는 마약 TOP 10'
+
+# error_message
+TABLE_NOT_EXIST = "Table does not exist"
+ROWS_NOT_EXIST = "rows does not exist"
+CHART_CREATION_REJECT = "차트 생성이 불가합니다"
+CHART_CREATION_FAILED = "차트 생성에 실패하였습니다"
--- apps/visualization/urls.py
+++ apps/visualization/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .constants import TOP10_PIE_CHART
+from .constants import TOP10_PIE_CHART, APP_NAME
 
-app_name = 'chart'
+app_name = APP_NAME
 urlpatterns = [
     path('top10/', views.top10_pie_chart, name=TOP10_PIE_CHART),
 ]
