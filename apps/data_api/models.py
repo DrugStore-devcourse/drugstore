42d95cf4e5c9f2995aad888797cc0d9fb8d33cb0
pjw74 <70009161+pjw74@users.noreply.github.com>
2023-05-03 14:42:40 +0900

    feat : create api db models

--- apps/data_api/models.py
+++ apps/data_api/models.py
@@ -1,25 +1,35 @@
 from django.db import models
-from django.utils import timezone
 
 
-# Create your models here.
 class Drug(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    id = models.AutoField(
+        primary_key=True,
+    )  # pk
+    drug_no = models.CharField(
+        max_length=3000, default="", null=False, verbose_name="약물번호"
+    )
+    drfstf = models.CharField(
+        max_length=3000, default="", null=False, verbose_name="한글명"
+    )
+    drfstf_eng = models.CharField(
+        max_length=3000, default="", null=False, verbose_name="영문명"
+    )
+    type_code = models.CharField(
+        max_length=3000, default="", null=False, verbose_name="분류/구분"
+    )
+    pharm = models.CharField(
+        max_length=3000, default="", null=True, verbose_name="이화학정보"
+    )
+    side_effect = models.CharField(
+        max_length=3000, default="", null=True, verbose_name="남용정보"
+    )
+    medication = models.CharField(
+        max_length=3000, default="", null=True, verbose_name="약물정보"
+    )
 
-    DRUG_NO = models.CharField(max_length=3000, default="")  # 약물번호
-    DRFSTF = models.CharField(max_length=3000, default="")  # 한글명
-    DRFSTF_ENG = models.CharField(max_length=3000, default="")  # 영문명
-    TYPE_CODE = models.CharField(max_length=3000, default="")  # 분류/구분
-    PHARM = models.CharField(max_length=3000, default="")  # 이화학정보
-    SIDE_EFFECT = models.CharField(max_length=3000, default="")  # 남용정보
-    MEDICATION = models.CharField(max_length=3000, default="")  # 약물정보
+    del_field = models.BooleanField(default=False, verbose_name="삭제여부")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일시")
 
-    DEL_FIELD = models.BooleanField("")  # 삭제여부
-    created_at = models.DateTimeField(timezone.now())  # 생성일시
-    updated_at = models.DateTimeField(timezone.now())  # 수정일시
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+    class Meta:
+        db_table = "drug"
