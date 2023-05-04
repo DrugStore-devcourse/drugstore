c370e28b2297fcb9acc2d24e2373a820d1e3d5d1
MACLSH <gracia10@daum.net>
2023-05-04 23:16:33 +0900

    feat : make command calling drug news crawler
    - 커맨드로 크롤러를 수행할 수 있도록 구현
    - runserver 시 크롤링 커맨드를 수행할 경우 HTTP 연결 실패하는 이슈 존재해 주석처리

--- apps/data_collection/apps.py
+++ apps/data_collection/apps.py
@@ -1,6 +1,21 @@
+import logging
+import sys
+
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class DataCollectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.data_collection'
+
+    def ready(self):
+        """
+         runserver시 마약 뉴스 크롤링 커맨드를 수행
+        """
+        # only run during 'runserver' command
+        if 'runserver' not in sys.argv:
+            return
+
+        # FIXME HTTP 연결 실패
+        # call_command('crawldata')
