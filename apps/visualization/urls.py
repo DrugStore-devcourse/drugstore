80a1de725c4baa6ab1a71e185ff79303ee708089
Nam Yoonah <nyah309@gmail.com>
2023-05-04 19:27:18 +0900

    feat: create word cloud(fix)

--- apps/visualization/urls.py
+++ apps/visualization/urls.py
@@ -5,4 +5,6 @@ from .constants import TOP10_PIE_CHART, APP_NAME
 app_name = APP_NAME
 urlpatterns = [
     path('top10/', views.top10_pie_chart, name=TOP10_PIE_CHART),
+    path('wordcloud/<int:id>/', views.wordcloud_chart, name='wordcloud_chart'),
+    
 ]
