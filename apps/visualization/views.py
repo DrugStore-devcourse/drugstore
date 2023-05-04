3d0ad733340fabfbf46fb22ab736874164962596
Nam Yoonah <nyah309@gmail.com>
2023-05-04 18:11:00 +0900

    feat: create word cloud

--- apps/visualization/views.py
+++ apps/visualization/views.py
@@ -3,14 +3,21 @@ import logging
 from pyecharts import options as opts
 from pyecharts.charts import Pie
 
+from wordcloud import WordCloud
+
+
 from django.shortcuts import render
 from django.http import Http404
 
 from apps.data_api.models import Drug
-from apps.data_collection.models import Word
+from apps.data_collection.models import *
 from apps.visualization.constants import *
 from django.db import connection
 
+from io import BytesIO
+import base64
+import os
+from django.conf import settings
 
 def top10_pie_chart(request):
     """
@@ -89,3 +96,51 @@ def _render_pie_chart(datas: list, title: str) -> dict:
     )
     chart = c.render_embed()
     return {'chart': chart}
+
+def drug_count(drug, article_id):       # 워드클라우드 생성을 위한 text 갯수 파악
+    count = {}
+    for id in article_id:
+        words = Word.objects.filter(article_id_id__exact=id)
+        for word in words:
+            if word.text not in count:
+                count[word.text] = word.frequecny
+            else:
+                count[word.text] += word.frequecny
+    return count
+
+
+def wordcloud_maker(count):        # 워드클라우드 생성
+
+    font_path = os.path.join(settings.BASE_DIR, 'apps',
+                             'visualization', 'static', 'font', 'A고딕18.TTF')
+
+    wc = WordCloud(font_path=font_path, background_color='white',
+                   max_words=1000, max_font_size=100).generate_from_frequencies(count)
+
+    # 워드클라우드 이미지를 BytesIO 객체에 저장
+    img_file = BytesIO()
+    wc.to_image().save(img_file, format='PNG')
+    img_file.seek(0)
+
+    # BytesIO 객체에 저장된 이미지 파일을 base64로 인코딩하여 웹 페이지에 전달
+    return base64.b64encode(img_file.read()).decode('utf-8')
+
+
+def wordcloud_chart(request, id):
+    try:
+        drug = Drug.objects.get(id__exact=id)
+        article_id = [word.article_id.article_id for word in Word.objects.filter(
+            text__exact=drug.drfstf)]
+
+        count = drug_count(drug, article_id)
+        img_base64 = wordcloud_maker(count)
+        context = {'img_base64': img_base64}
+
+    except (Drug.DoesNotExist, Word.DoesNotExist, AttributeError):
+        raise Http404(CHART_CREATION_REJECT)
+    except FileNotFoundError:
+        context = {'error_message': FILE_NOT_FOUND}
+    except:
+        context = {'error_message': CHART_CREATION_FAILED}
+
+    return render(request, 'visualization/wordcloud.html', context)
