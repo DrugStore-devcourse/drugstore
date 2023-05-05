26341dda5d222de1ddb243ff3c69369b1dbc8d2d
HyeM207 <hmk9667@gmail.com>
2023-05-05 22:18:07 +0900

    fix: Allow call_command
    Delete comment of call_command()

--- apps/data_collection/apps.py
+++ apps/data_collection/apps.py
@@ -11,11 +11,10 @@ class DataCollectionConfig(AppConfig):
 
     def ready(self):
         """
-         runserver시 마약 뉴스 크롤링 커맨드를 수행
+        runserver시 마약 뉴스 크롤링 커맨드를 수행
         """
         # only run during 'runserver' command
         if 'runserver' not in sys.argv:
             return
 
-        # FIXME HTTP 연결 실패
-        # call_command('crawldata')
+        call_command('crawldata')
