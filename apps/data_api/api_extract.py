90b713edd03ba7ccf38f435a6bcef18f3e0ef389
MACLSH <gracia10@daum.net>
2023-05-06 02:09:54 +0900

    hotfix : api 데이터 로드 스크립트 수정
    - JSON 파일 생성 위치 수정
    - apps/data_api/drug.json 삭제
    - 중복 데이터 적재 수정
     # (예시) Close #5

--- apps/data_api/api_extract.py
+++ apps/data_api/api_extract.py
@@ -1,8 +1,12 @@
-from env import key
+import os
+
 import requests
 import json
 import pandas as pd
 from datetime import datetime
+
+from drugstore import settings
+from env.keys import API_CONFIG
 
 """
 openAPI로부터 drugs 데이터 추출
@@ -15,19 +19,20 @@ openAPI로부터 drugs 데이터 추출
 - null인 경우 : ""
 """
 
+
 def get_All_Data(key, url):
     data = []
-    params = {'serviceKey' : key, 'pageNo' : '1', 'numOfRows' : '1', 'Drfstf' : '', 'Drfstf_eng' : '', 'type' : 'json'}
-    
+    params = {'serviceKey': key, 'pageNo': '1', 'numOfRows': '1', 'Drfstf': '', 'Drfstf_eng': '', 'type': 'json'}
+
     # 전체 데이터 수 탐색
     loading = json.loads(requests.get(url, params=params).text)
     cnt = loading['body']['totalCount']
-    
+
     pages = (cnt // 100) + 1
     params['numOfRows'] = str(100)
     loading['body']['items'].pop()
     all_json = loading
-    
+
     for i in range(1, pages + 1):
         if i == pages:
             params['numOfRows'] = str(cnt % 100)
@@ -37,14 +42,17 @@ def get_All_Data(key, url):
     all_json['body']['items'] = data
     return data, all_json
 
+
 def raw_2_modelformat(all_data):
-    all_data1 = all_data[:]
+    # 4개 fields 추가
+    all_data1 = all_data
     for i in range(len(all_data1)):
         all_data1[i]['DEL_FIELD'] = False
-        all_data1[i]['ID'] = i+1
+        all_data1[i]['ID'] = i + 1
         all_data1[i]['created_at'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         all_data1[i]['updated_at'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        
+
+    # fields명 소문자로 변경
     all_data2 = []
     for i in range(len(all_data1)):
         dic = {}
@@ -52,22 +60,39 @@ def raw_2_modelformat(all_data):
             lowkey = key.lower()
             dic[lowkey] = all_data1[i][key]
         all_data2.append(dic)
-    
-    jslist = []
-    for i in range(len(all_data1)):
-        dic = {"model":'data_api.Drug', 'fields':all_data2[i]}
-        jslist.append(dic)
-    
-    with open('./drug.json', 'w', encoding='UTF-8-sig') as f:
-        json.dump(jslist, f, indent=4,ensure_ascii=False)
-    
 
-def main():
-    apikey = key.API_CONFIG['key']
-    url = key.API_CONFIG['url']
+    # 결측치 처리, 중복 제거, fields 순서 변경
+    df = pd.DataFrame(all_data2)
+    df = df.fillna("")
+    df = df.drop_duplicates(['drfstf'], keep='last', ignore_index=True)
+    df = df[['id', 'drug_no', 'drfstf', 'drfstf_eng', 'type_code', 'pharm', 'side_effect', 'medication', 'del_field',
+             'created_at', 'updated_at']]
+    all_data3 = df.to_dict('index')
+
+    # format 변경
+    jslist = []
+    for i in range(len(all_data3)):
+        dic = {"model": 'data_api.Drug', "fields": all_data3[i]}
+        jslist.append(dic)
+
+    fixture_dir = os.path.join(settings.BASE_DIR, 'drugstore', 'fixtures')
+    file_path = os.path.join(fixture_dir, 'drug.json')
+
+    with open(file_path, 'w', encoding='UTF-8') as f:
+        json.dump(jslist, f, indent=2, ensure_ascii=False)
+
+
+def run():
+    apikey = API_CONFIG['key']
+    url = API_CONFIG['url']
 
     all_data, all_json = get_All_Data(apikey, url)
     raw_2_modelformat(all_data)
+
+
+def main():
+    run()
+
 
 if __name__ == "__main__":
     main()
